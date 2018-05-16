3f122b8767d7dbde44d79be0a26aa30e5a9b663e
Langzi418 <1677360593@qq.com>
2018-05-16 23:22:51 +0800

    login

---
+++ config/users.js
@@ -0,0 +1,9 @@
+// 模拟用户
+module.exports = {
+  guest: '123456',
+  admin: '123456'
+  // users: [
+  //   { username: guest, password: guest },
+  //   { username: admin, password: admin }
+  // ]
+}
