f6a0898b2ed04c68153c2d50f860ff3a1b193868
Langzi418 <1677360593@qq.com>
2018-05-19 09:25:52 +0800

    test

--- utils/iplocation.js
+++ utils/iplocation.js
@@ -2,6 +2,10 @@ const fetch = require('node-fetch')
 
 module.exports = {
   location: ip => {
+    if (ip.substr(0, 7) === '::ffff:') {
+      ip = ip.substr(7)
+    }
+
     var url = `http://api.map.baidu.com/location/ip?ip=${ip}&ak=FGRlCEw0t4iwksHiRXEZZYgC3rswhkvK&coor=bd09ll`
     return fetch(url)
   }
