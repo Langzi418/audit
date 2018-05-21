4be77b034192d9a84274a32aa6d5126f8ccd9d5f
WangZilei <2292056398@qq.com>
2018-05-21 15:31:10 +0800

    【添加】后台接口

---
+++ showLog/log-sum/application/controllers/Log_data.php
@@ -0,0 +1,39 @@
+<?php
+
+class Log_data extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('mongodb');
+		header("Access-Control-Allow-Origin:*");
+		header("Access-Control-Allow-Methods:GET,POST");
+	}
+
+
+	public function get_all_log()
+	{
+		$res = get_all_log();
+		$this->output->set_content_type('application/json')->set_output(json_encode($res));
+	}
+
+	public function get_log_by_ip()
+	{
+		$ip = $this->input->get('ip', TRUE);
+		$res = get_log_by_ip($ip);
+		$this->output->set_content_type('application/json')->set_output(json_encode($res));
+	}
+
+	public function get_log_by_agent()
+	{
+		$ip = $this->input->get('agent', TRUE);
+		$res = get_log_by_agent($agent);
+		$this->output->set_content_type('application/json')->set_output(json_encode($res));
+	}
+
+	public function get_log_by_time()
+	{
+		$ip = $this->input->get('time', TRUE);
+		$res = get_log_by_time($time);
+		$this->output->set_content_type('application/json')->set_output(json_encode($res));
+	}
+}
---
+++ showLog/log-sum/application/helpers/mongodb_helper.php
@@ -0,0 +1,53 @@
+<?php
+
+define('DB_NAME','test');
+define('COLLECTION_NAME','ppt');
+define("DB_CONNECTION","mongodb://localhost:27017");
+
+function obj2array($log_data)
+{
+	$res = array();
+	foreach ($log_data as $item) {
+		$res[] = $item;
+	}
+	return $res;
+}
+
+function get_all_log()
+{
+	$manager = new MongoDB\Driver\Manager(DB_CONNECTION);
+	$query = new MongoDB\Driver\Query([], []);
+	$cursor = $manager->executeQuery(DB_NAME.".".COLLECTION_NAME, $query);
+	$res = obj2array($cursor);
+	return $res;
+}
+
+function get_log_by_ip($ip)
+{
+	$manager = new MongoDB\Driver\Manager(DB_CONNECTION);
+	$filter = array('remote_addr' => (int)$ip);
+	$query = new MongoDB\Driver\Query($filter, []);
+	$cursor = $manager->executeQuery(DB_NAME.".".COLLECTION_NAME, $query);
+	$res = obj2array($cursor);
+	return $res;
+}
+
+function get_log_by_agent($agent)
+{
+	$manager = new MongoDB\Driver\Manager(DB_CONNECTION);
+	$filter = array('http_user_agent' => $agent);
+	$query = new MongoDB\Driver\Query($filter, []);
+	$cursor = $manager->executeQuery(DB_NAME.".".COLLECTION_NAME, $query);
+	$res = obj2array($cursor);
+	return $res;
+}
+
+function get_log_by_time($time)
+{
+	$manager = new MongoDB\Driver\Manager(DB_CONNECTION);
+	$filter = array('time_local' => $time);
+	$query = new MongoDB\Driver\Query($filter, []);
+	$cursor = $manager->executeQuery(DB_NAME.".".COLLECTION_NAME, $query);
+	$res = obj2array($cursor);
+	return $res;
+}
